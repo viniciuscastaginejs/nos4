a9cb12a02b04a36cba3803f1328f4e7493a8de67
Vinicius Castagine <viniciuscastagine1@gmail.com>
2026-04-20 09:23:50 -0300

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,9 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-
-console.log('URL:', supabaseUrl)
-console.log('KEY:', supabaseKey)
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(
+  'https://rfdtfkqkxsqylagujmcv.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmZHRma3FreHNxeWxhZ3VqbWN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUxNDY4NTEsImV4cCI6MjA5MDcyMjg1MX0.ndlZCBcrfbWBIWzbfdwpx3YGZ9n5MnLZIRcSiKVDv38'
+)
